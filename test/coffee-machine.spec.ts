1705065d296ba057913e0cea66335d10c9079bc5
ilyasshabi <ilyasshabi94@gmail.com>
2020-03-08 13:59:12 +0100

    Add extra hot drinks tests

--- test/coffee-machine.spec.ts
+++ test/coffee-machine.spec.ts
@@ -34,5 +34,25 @@ describe('Coffee Machine test cases', () => {
 		expect(result).toEqual("Drink: juice - Sugar: 0 - Stick: 0")
 	});
 
+	it('Extra hot Tea with 3 sugars and a stick => T:3:0', () => {
+
+		const commande  = new Commande("Th:3:0", 1);
+        const result = commande.getCommande();
+        expect(result).toEqual("Drink: extra hot tea - Sugar: 3 - Stick: 1");
+        
+	});
+
+	it('Extra hot Chocolate with one sugar and one stick => H:1:0', () => {
+        const commande  = new Commande("H:1:0", 1);
+        const result = commande.getCommande();
+		expect(result).toEqual("Drink: extra hot chocolate - Sugar: 1 - Stick: 1");
+	});
+
+	it('Extra hot Coffee with 2 sugars => C:2:0', () => {
+        const commande  = new Commande("C:2:0", 1);
+        const result = commande.getCommande();
+		expect(result).toEqual("Drink: extra hot coffee - Sugar: 2 - Stick: 1")
+	});
+
 });
 
